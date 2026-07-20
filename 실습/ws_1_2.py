f1302a69fcced162a49217170c9b8a2da5f86c9f
rkdwnstj0326 <rkdwnstj0326@gmail.com>
2026-07-20 17:43:01 +0900

    20260720 실습

---
+++ 실습/ws_1_2.py
@@ -0,0 +1,23 @@
+string = '문자열'
+integer = 10
+floating_point = 3.14
+a_list = [1, 2, 3, 4, 5]
+dictionary = {'name': '홍길동', 'age': 20}
+a_set = {1, 2, 3, 4, 5}
+a_range = range(11)
+a_tuple = (1, 2, 3)
+boolean = True
+
+print()
+
+# 주어진 데이터들의 data type를 구해야함
+# data type는 type를 활용해서 구할 수 있음
+print(type(string))
+print(type(integer))
+print(type(floating_point))
+print(type(a_list))
+print(type(dictionary))
+print(type(a_set))
+print(type(a_range))
+print(type(a_tuple))
+print(type(boolean))
