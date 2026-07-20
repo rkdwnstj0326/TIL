a7c40bfe43f87de340feb9780dad5d9e75697dbe
rkdwnstj0326 <rkdwnstj0326@gmail.com>
2026-07-20 12:30:09 +0900

    junsi

---
+++ 실습/hw_1_4.py
@@ -0,0 +1,14 @@
+# 사과는 영어로 apple
+# 바나나는 영어로 banana
+# 키위는 영어로 kiwi
+
+nini = "사과는 영어로 apple"
+nana = "바나나는 영어로 banana"
+wiwi = "키위는 영어로 kiwi"
+
+print(nini)
+print(nana)
+print(wiwi)
+
+print(nini + "\n" + nana + "\n" + wiwi)
+
