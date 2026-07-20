a7275b0722abfad5c4d77374d5714737ceb5e118
rkdwnstj0326 <rkdwnstj0326@gmail.com>
2026-07-20 17:15:35 +0900

    20260720 실습정리

---
+++ 실습/ws_1_1.py
@@ -0,0 +1,14 @@
+name = '홍길동'
+age = 20
+the_name = '이름 : '
+the_age = '나이 : '
+stu_info= '학생 정보를 출력합니다.'
+info = '각각의 정보는 다음과 같습니다'
+finish = '출력 완료'
+
+print(stu_info)
+print(info)
+print(the_name, name)
+print(the_age, age)
+print(finish)
+
